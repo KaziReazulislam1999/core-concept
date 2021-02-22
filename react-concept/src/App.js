fe87796dc2e2e8fae492d199cf764c379baba16a
Kazi Reazul Islam <kazireazul1999@gmail.com>
2021-02-23 01:56:27 +0600

    update app.js

--- react-concept/src/App.js
+++ react-concept/src/App.js
@@ -2,6 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  let person = {
+    name: "Reazul",
+    profession: "Developer",
+    age: 21,
+  };
+
+  let style = {
+    backgroundColor: "green",
+    color: "Red",
+    padding: 5,
+  };
+
   return (
     <div className='App'>
       <header className='App-header'>
@@ -9,6 +21,11 @@ function App() {
         <p>
           Edit KORO <code>src/App.js</code> and save to reload.
         </p>
+        <h2 style={{ backgroundColor: "orange", color: "#fff", padding: 10 }}>
+          My Name is {person.name} Islam{" "}
+        </h2>
+        <p style={style}>I am a {person.profession}</p>
+        <p>I am {person.age} Years Old</p>
         <a
           className='App-link'
           href='https://reactjs.org'
