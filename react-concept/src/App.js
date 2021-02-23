92bbe802e669537761020ca45e0ff1e6714f2230
Kazi Reazul Islam <kazireazul1999@gmail.com>
2021-02-23 20:42:41 +0600

    dynamic name and foods

--- react-concept/src/App.js
+++ react-concept/src/App.js
@@ -2,6 +2,8 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const friends = ['korim','jamal','nojju', 'kooka','kobooo']
+  const foods = ['AluVorta','Dalvaji','DimVuna','DimShutki','MurgiVorta']
   return (
     <div className='App'>
       <header className='App-header'>
@@ -10,11 +12,11 @@ function App() {
           Edit KORO <code>src/App.js</code> and save to reload.
         </p>
 
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
+        <Person name={friends[0]} age="21" food={foods[0]}></Person>
+        <Person name={friends[1]} age="30" food= {foods[1]}></Person>
+        <Person name={friends[2]} age="31" food={foods[2]}></Person>
+        <Person name={friends[3]} age="32" food={foods[3]}></Person>
+        <Person name={friends[4]} age="33" food={foods[4]}></Person>
 
         <a
           className='App-link'
@@ -29,17 +31,20 @@ function App() {
   );
 }
 
-function Person() {
+function Person(props) {
  let personStyle={
-  border:'2px solid green',
   backgroundColor:'red' ,
-  borderRadius:'20px'
+  borderRadius:'20px',
+  border:'none',
+  padding:'20px',
+  width:"400px",
+  margin:'10px'
 
  }
 
   return ( <div style={personStyle}>
-    <h1>Name : Shakib Al Hasan</h1>
-    <h3>Bangladesher Pran</h3>
+    <h1>Name : {props.name}</h1>
+    <h3>Favorite Food {props.food}</h3>
   </div>)
 
 }
